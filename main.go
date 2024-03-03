3321d33a9c5fdcc4769bff1fcc5aa65bd1ebf9c6
doanmt <doanmt.it@gmail.com>
2024-03-03 10:46:36 +0700

    Refactor: remove main.go

--- main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-// func main() {
-// 	router := gin.Default()
-// 	router.GET("/", func(ctx *gin.Context) {
-// 		ctx.JSON(http.StatusOK, gin.H{"data": "Hello, world!"})
-// 	})
-// 	router.Run(":8000")
-// }
